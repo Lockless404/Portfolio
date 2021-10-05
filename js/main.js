e8c6a3091472964128df26937121d04e6979c9d2
Hamza <hamzatarar8@gmail.com>
2021-10-05 11:19:39 +0200

    Added Linters

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const nav = document.querySelector('.navLst');
 const linkedWord = document.querySelectorAll('.navWord');
 
 const navAppear = () => {
-
   menuIcon.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     menuIcon.classList.toggle('turn');
@@ -12,11 +11,11 @@ const navAppear = () => {
 
 const navDisappear = () => { 
   for (let i = 0; i < linkedWord.length; i++) { 
-    linkedWord[i].addEventListener('click', () => 
-    { nav.classList.remove('nav-active'); 
-    menuIcon.classList.remove('turn'); 
-  }) 
-} 
+    linkedWord[i].addEventListener('click', () => {
+       nav.classList.remove('nav-active'); 
+      menuIcon.classList.remove('turn'); 
+    }) 
+  } 
 } 
 navDisappear(); 
 navAppear();
