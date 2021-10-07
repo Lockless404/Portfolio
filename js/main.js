b68ffa0fff91809cf39e51ae6e6652b6264282c7
Hamza <hamzatarar8@gmail.com>
2021-10-07 11:21:22 +0200

    Corrected linter errors.

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ const menuIcon = document.querySelector('.menuIcon');
 const nav = document.querySelector('.navLst');
 const linkedWord = document.querySelectorAll('.navWord');
 const modal = document.querySelector('.modals');
-const modal1 = document.querySelector('.modal1');
-let imageLink = document.querySelector('.modalImage').outerHTML;
-const button = document.querySelector('.cardBtn');
+//const modal1 = document.querySelector('.modal1');
+//let imageLink = document.querySelector('.modalImage').outerHTML;
+//const button = document.querySelector('.cardBtn');
 const buttonOne = document.querySelector('.cardBtn.one');
 const buttonTwo = document.querySelector('.cardBtn.two');
 const buttonThree = document.querySelector('.cardBtn.three');
@@ -13,7 +13,7 @@ const buttonFive = document.querySelector('.cardBtn.five');
 const buttonSix = document.querySelector('.cardBtn.six');
 const projectButton = document.querySelector('.seeButton');
 const modalCloseButton = document.querySelector('.modalButtonClose');
-let heading = document.querySelector('.modalHeading').innerHTML;
+//let heading = document.querySelector('.modalHeading').innerHTML;
 
 const navAppear = () => {
   menuIcon.addEventListener('click', () => {
@@ -37,60 +37,60 @@ const popUp1 = () => {
   });
 };
 
-let projectCollection = {
+const projectCollection = {
   project1: {
     heading: 'Multi Post Stories',
     tag1: 'html',
     tag3: 'Bootstrap',
     tag2: 'Ruby on rails',
-    imageLink: "<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>",
-    paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    imageLink: '<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>',
+    paragraph: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
   },
   project2: {
     heading: 'Empty',
     tag1: 'Empty',
     tag3: 'Empty',
     tag2: 'Empty',
-    imageLink: "<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>",
-    paragraph: "Empty"
+    imageLink: '<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>',
+    paragraph: 'Empty',
   },
   project3: {
     heading: 'Blah',
     tag1: 'Blah',
     tag3: 'Blah',
     tag2: 'Blah',
-    imageLink: "<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>",
-    paragraph: "Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah"
+    imageLink: '<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>',
+    paragraph: 'Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah',
   },
   project4: {
     heading: 'Meh',
     tag1: 'Meh',
     tag3: 'Meh',
     tag2: 'Meh',
-    imageLink: "<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>",
-    paragraph: "Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh"
+    imageLink: '<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>',
+    paragraph: 'Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh Meh',
   },
   project5: {
     heading: 'Boohoo',
     tag1: 'Boohoo',
     tag3: 'Boohoo',
     tag2: 'Boohoo',
-    imageLink: "<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>",
-    paragraph: "Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo"
+    imageLink: '<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>',
+    paragraph: 'Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo Boohoo',
   },
   project6: {
     heading: 'Kak',
     tag1: 'Kak',
     tag3: 'Kak',
     tag2: 'Kak',
-    imageLink: "<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>",
-    paragraph: "Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak"
-  }
+    imageLink: '<div class=\"modalImage\"><img class=\"noTenImg\" src=\"images/tenBig.png\" alt=\"Big Number 10\"></div>',
+    paragraph: 'Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak Kak',
+  },
 };
 const popUpAppear = () => {
   buttonOne.addEventListener('click', () => {
     modal.classList.add('modal-appear');
-    document.querySelector('.modalHeading').innerHTML = 'adadadaddad';
+    document.querySelector('.modalHeading').innerHTML = projectCollection.project1.heading;
     document.querySelector('.modalbadge.one').innerHTML = projectCollection.project1.tag1;
     document.querySelector('.modalbadge.two').innerHTML = projectCollection.project1.tag2;
     document.querySelector('.modalbadge.three').innerHTML = projectCollection.project1.tag3;
@@ -142,12 +142,12 @@ const popUpAppear = () => {
   });
 };
 
-popUpDisappear = () => { 
-  modalCloseButton.addEventListener('click', () => { 
-    modal.classList.remove('modal-appear'); 
-  }); 
-}; 
-popUpDisappear(); 
+const popUpDisappear = () => {
+  modalCloseButton.addEventListener('click', () => {
+    modal.classList.remove('modal-appear');
+  });
+};
+popUpDisappear();
 navDisappear();
 navAppear();
 popUpAppear();
