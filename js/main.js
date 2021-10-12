655c2e58e1fdf2797f9d29d402e484133b2eb9f8
Hamza <hamzatarar8@gmail.com>
2021-10-12 17:04:33 +0200

    Called function earlier.

--- js/main.js
+++ js/main.js
@@ -35,7 +35,8 @@ const navAppear = () => {
     nav.classList.toggle('nav-active');
     menuIcon.classList.toggle('turn');
   });
-};
+};;
+navAppear();
 
 const navDisappear = () => {
   for (let i = 0; i < linkedWord.length; i += 1) {
@@ -45,6 +46,7 @@ const navDisappear = () => {
     });
   }
 };
+navDisappear()
 
 const addData = (formName, formEmail, formMsg) => {
   const myStorage = { formName, formEmail, formMsg };
@@ -73,5 +75,3 @@ const displayData = () => {
 displayData();
 submission();
 showError();
-navDisappear();
-navAppear();
